2f068eb06e0a465811138204388ec0d6f5dd81a4
Arthur Koba <caprimores@gmail.com>
2025-06-13 11:27:41 +0300

    updated app

--- src/app.cpp
+++ src/app.cpp
@@ -1,41 +1,158 @@
 #include "app.h"
 
-#include "audio/loopback/factory.h"
-
 Application::Application(int &argc, char **argv, int flags) : QApplication(argc, argv, flags) {
 
-    loopback = audio::loopback::LoopbackFactory::get_loopback();
+    if (_cfg.run_analyser) {
+        _run_analyzer();
+    }
 
-    samplesChartView1 = new SamplesChartView;
-    samplesChartView2 = new SamplesChartView;
-//    amplitudesChartView = new AmplitudesChartView;
+    if (_cfg.run_loopback) {
+        _run_loopback();
+    }
 
-    loopback->set_audio_handler([this] (audio::loopback::loopback_audio data) {
-        Packet packet1;
-        packet1.id = 10;
-        packet1.size = data.samples;
-        packet1.data_ptr = data.data;
-        Packet packet2 = packet1;
-        packet2.data_ptr = data.data + data.samples;
-        samplesChartView1->execPacket(packet1);
-        samplesChartView2->execPacket(packet2);
-    });
-
+    if (_cfg.run_generator) {
+        _run_generator();
+    }
 
     splitter = new QSplitter;
     splitter->setOrientation(Qt::Vertical);
 
-    splitter->addWidget(samplesChartView1);
-    splitter->addWidget(samplesChartView2);
+    if (_cfg.show_raw_samples and loopback) {
+        rawSamplesView = new FPSChartView();
+        rawSamplesView->axisY.setRange(-1, 1);
+        rawSamplesView->chart()->setTitle("Raw samples");
+        splitter->addWidget(rawSamplesView);
+    } else if (_cfg.show_raw_samples) {
+        qInfo("Raw samples only can show when loopback running");
+    }
+
+    if (_cfg.show_generator_samples and generator) {
+        generatorSamplesView = new FPSChartView();
+        generatorSamplesView->axisY.setRange(-1, 1);
+        generatorSamplesView->chart()->setTitle("Generator samples");
+        splitter->addWidget(generatorSamplesView);
+    } else if (_cfg.show_generator_samples) {
+        qInfo("Generator samples only can show when generator running");
+    }
+
+    if (_cfg.show_samples and analyzer) {
+        samplesView = new FPSChartView();
+        samplesView->axisY.setRange(-1, 1);
+        samplesView->chart()->setTitle("Analyzer samples");
+        splitter->addWidget(samplesView);
+    } else if (_cfg.show_samples) {
+        qInfo("Analyzer samples only can show when analyzer running");
+    }
+
+    if (_cfg.show_amplitudes and analyzer) {
+        amplitudesView = new FPSChartView();
+        amplitudesView2 = new FPSChartView();
+
+        amplitudesView->axisY.setRange(-0.10, 1.2);
+        amplitudesView->chart()->setTitle("Amplitudes FFT 2-radix");
+        amplitudesView2->axisY.setRange(-0.10, 1.2);
+        amplitudesView2->chart()->setTitle("Amplitudes FFT 4-radix");
+
+        splitter->addWidget(amplitudesView);
+        splitter->addWidget(amplitudesView2);
+    } else if (_cfg.show_amplitudes) {
+        qInfo("Analyzer amplitudes only can show when analyzer running");
+    }
 
     window.setCentralWidget(splitter);
-    window.resize(800, 400);
+    window.resize(1200, 800);
 //    window.grabGesture(Qt::PanGesture);
 //    window.grabGesture(Qt::PinchGesture);
-
-    loopback->start();
     window.show();
 }
 
 Application::~Application() {
+    if (generator) {
+        generator->stop();
+        qInfo("Generator stopped successfully");
+    }
+    if (analyzer) {
+        analyzer->stop();
+        qInfo("Analyzer stopped successfully");
+    }
+    if (loopback) {
+        loopback->set_audio_handler(nullptr);
+        loopback->stop();
+        qInfo("Loopback stopped successfully");
+    }
+}
+
+void Application::_run_analyzer() {
+    if (analyzer) {
+        return qDebug("Analyzer already started");
+    }
+
+    analyzer = new Analyzer;
+
+    analyzer->set_update_handler([this]() {
+        if (samplesView) {
+            samplesView->update(analyzer->samples.left);
+        }
+
+        if (amplitudesView) {
+            amplitudesView->update(analyzer->amplitudes.left);
+        }
+        if (amplitudesView2) {
+            amplitudesView2->update(analyzer->amplitudes.right);
+        }
+    });
+
+    analyzer->start();
+}
+
+
+void Application::_run_loopback() {
+    if (loopback) {
+        return qDebug("Loopback already started");
+    }
+    if (not analyzer) {
+        return qDebug("Analyser not exist");
+    }
+
+    loopback = audio::loopback::LoopbackFactory::get_loopback();
+
+    if (not loopback) {
+        return qCritical("Failed get loopback from Factory");
+    }
+
+    loopback->set_audio_handler([this](audio::loopback::loopback_audio data) {
+        Samples samples(data.samples);
+        for (int i = 0; i < data.samples; ++i) {
+            samples.left[i] = data.data[i];
+            samples.right[i] = data.data[i + data.samples];
+        }
+        if (_cfg.show_raw_samples and rawSamplesView) {
+            rawSamplesView->update(samples.left);
+        }
+        if (analyzer) {
+            analyzer->add_samples(samples);
+        }
+    });
+
+    loopback->start();
+}
+
+void Application::_run_generator() {
+    if (generator) {
+        return qDebug("Generator already started");
+    }
+
+    generator = new Generator;
+
+    generator->set_handler([this](std::vector<float> data) {
+        Samples samples(data.size());
+        for (int i = 0; i < data.size(); ++i) {
+            samples.left[i] = data[i];
+            samples.right[i] = data[i];
+        }
+        if (generatorSamplesView) {
+            generatorSamplesView->update(samples.left);
+        }
+    });
+    generator->start();
 }
--- src/app.h
+++ src/app.h
@@ -5,31 +5,42 @@
 #include <QMainWindow>
 #include <QSplitter>
 
-#include "charts/samples.h"
-//#include "charts/amplitudes.h"
-
-//#include "BDSP.h"
-//#include "communication/serial.h"
-
-//#include "wasapi.h"
-#include "audio/loopback/types.h"
+#include "analyzer/analyzer.h"
+#include "analyzer/generator/generator.h"
+#include "loopback/factory.h"
+#include "charts/fps.h"
 
 class Application : public QApplication {
 public:
-
-    SamplesChartView *samplesChartView1 = nullptr;
-    SamplesChartView *samplesChartView2 = nullptr;
-//    AmplitudesChartView *amplitudesChartView = nullptr;
-
-//    Serial serial;
+    struct Config {
+        bool run_loopback = true;
+        bool run_generator = false;
+        bool run_analyser = true;
+        bool show_generator_samples = false;
+        bool show_raw_samples = false;
+        bool show_samples = false;
+        bool show_amplitudes = true;
+    };
 
     Application(int &argc, char **argv, int = ApplicationFlags);
-    ~Application();
+    ~Application() override;
 private:
-    audio::loopback::IAudioLoopback *loopback;
+    Config _cfg;
+
+    FPSChartView *rawSamplesView = nullptr;
+    FPSChartView *generatorSamplesView = nullptr;
+    FPSChartView *samplesView = nullptr;
+    FPSChartView *amplitudesView = nullptr;
+    FPSChartView *amplitudesView2 = nullptr;
+    Analyzer *analyzer = nullptr;
+    Generator *generator = nullptr;
+    audio::loopback::IAudioLoopback *loopback = nullptr;
     QSplitter *splitter;
     QMainWindow window;
-//    BDSPReceiver receiver;
+
+    void _run_analyzer();
+    void _run_generator();
+    void _run_loopback();
 };
 
 
